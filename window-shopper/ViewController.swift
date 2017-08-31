970d5cc309c1c68c47bad2d890faaf7a801debed
Ding Zhiyi <minerva@DingdeMacBook-Pro.local>
2017-09-01 01:53:25 +0900

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Ding Zhiyi on 01/09/2017.
+//  Copyright © 2017 Ding Zhiyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
